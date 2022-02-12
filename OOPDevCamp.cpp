768fa021d2d570dd0a07706ffd34c1ada52a8c42
Viktor Andrijevski <viktor.andrijevski@mentormate.com>
2022-02-12 15:11:55 +0200

    v2

--- OOPDevCamp.cpp
+++ OOPDevCamp.cpp
@@ -5,12 +5,13 @@ using namespace std;
 
 class Shape {
 public:
-	virtual double calculateSurface() = 0;
-	virtual double calculatePerimeter() = 0;
+	virtual double getSurface() { return 0; };
+	virtual double getPerimeter() { return 0; }
 };
 
 class Shape3D : public Shape {
-	virtual double calculateVolume() = 0;
+public:
+	virtual double getVolume() { return 0; }
 };
 
 class Triangle : public Shape {
@@ -21,11 +22,23 @@ public:
 		side3 = z;
 		s = (x + y + z) / 2;
 	}
+	virtual double getPerimeter() {
+		return calculatePerimeter();
+	}
+	virtual double getSurface() {
+		return calculateSurface();
+	}
+	bool getIfValidTriangle() {
+		return isValidTriangle();
+	}
+	string getTypeOfTriangle() {
+		return typeOfTriangle();
+	}
+private:
 	bool isValidTriangle();
 	virtual double calculatePerimeter();
 	virtual double calculateSurface();
 	string typeOfTriangle();
-private:
 	double side1, side2, side3, s;
 };
 
@@ -40,7 +53,7 @@ double Triangle::calculateSurface() {
 bool Triangle::isValidTriangle() {
 	if (side1 + side2 > side3 && side2 + side3 > side1 && side1 + side3 > side2)
 		return true;
-	else 
+	else
 		return false;
 }
 
@@ -62,9 +75,15 @@ public:
 		side1 = x;
 		side2 = y;
 	}
+	virtual double getSurface() {
+		return calculateSurface();
+	}
+	virtual double getPerimeter() {
+		return calculatePerimeter();
+	}
+private:
 	virtual double calculatePerimeter();
 	virtual double calculateSurface();
-private:
 	double side1, side2;
 };
 
@@ -78,12 +97,18 @@ double Rectangle::calculateSurface() {
 
 class Square : public Shape {
 public:
-	Square (double x) {
+	Square(double x) {
 		side = x;
 	}
+	virtual double getPerimeter() {
+		return calculatePerimeter();
+	}
+	virtual double getSurface() {
+		return calculateSurface();
+	}
+private:
 	virtual double calculatePerimeter();
 	virtual double calculateSurface();
-private:
 	double side;
 };
 
@@ -100,9 +125,15 @@ public:
 	Circle(double x) {
 		radius = x;
 	}
+	virtual double getPerimeter() {
+		return calculatePerimeter();
+	}
+	virtual double getSurface() {
+		return calculateSurface();
+	}
+private:
 	virtual double calculatePerimeter();
 	virtual double calculateSurface();
-private:
 	double radius;
 };
 
@@ -119,10 +150,19 @@ public:
 	Cube(double x) {
 		side = x;
 	}
+	virtual double getPerimeter() {
+		return calculatePerimeter();
+	}
+	virtual double getSurface() {
+		return calculateSurface();
+	}
+	virtual double getVolume() {
+		return calculateVolume();
+	}
+private:
 	virtual double calculatePerimeter();
 	virtual double calculateSurface();
 	virtual double calculateVolume();
-private:
 	double side;
 };
 
@@ -137,26 +177,93 @@ double Cube::calculateSurface() {
 double Cube::calculateVolume() {
 	return pow(side, 3);
 }
+
+void printMenu() {
+	cout << "1 - Triangle" << endl;
+	cout << "2 - Rectangle" << endl;
+	cout << "3 - Square" << endl;
+	cout << "4 - Circle" << endl;
+	cout << "5 - Cube" << endl;
+}
+
+int getUserChoice() {
+	int index; cout << "Enter shape index: "; cin >> index;
+	return index;
+}
+
+Triangle getTriangle() {
+	double side1, side2, side3;
+	cout << "Enter first side: "; cin >> side1;
+	cout << "Enter second side: "; cin >> side2;
+	cout << "Enter third side: "; cin >> side3;
+	return Triangle(side1, side2, side3);
+}
+
+void printTriangleInfo(Triangle t) {
+	if (t.getIfValidTriangle()) {
+		cout << "Perimeter: " << t.getPerimeter() << endl;
+		cout << "Surface: " << t.getSurface() << endl;
+		cout << "Type of triangle: " << t.getTypeOfTriangle() << endl;
+	}
+	else cout << "Not a valid triangle!" << endl;
+}
+
+Rectangle getRectangle() {
+	double side1, side2;
+	cout << "Enter first side: "; cin >> side1;
+	cout << "Enter second side: "; cin >> side2;
+	return Rectangle(side1, side2);
+}
+
+void printRectangleInfo(Rectangle r) {
+	cout << "Perimeter: " << r.getPerimeter() << endl;
+	cout << "Surface: " << r.getSurface() << endl;
+}
+
+Square getSquare() {
+	double side;
+	cout << "Enter side: "; cin >> side;
+	return Square(side);
+}
+
+void printSquareInfo(Square s) {
+	cout << "Perimeter: " << s.getPerimeter() << endl;
+	cout << "Surface: " << s.getSurface() << endl;
+}
+
+Circle getCircle() {
+	double radius;
+	cout << "Enter radius: "; cin >> radius;
+	return Circle(radius);
+}
+
+void printCircleInfo(Circle c) {
+	cout << "Perimeter: " << c.getPerimeter() << endl;
+	cout << "Surfce: " << c.getSurface() << endl;
+}
+
+Cube getCube() {
+	double side;
+	cout << "Enter side: "; cin >> side;
+	return Cube(side);
+}
+
+void printCubeInfo(Cube c) {
+	cout << "Perimeter: " << c.getPerimeter() << endl;
+	cout << "Surfce: " << c.getSurface() << endl;
+	cout << "Volume: " << c.getVolume() << endl;
+}
+
 int main()
 {
-	Triangle firstTriangle(2, 4, 3);
-	Triangle secondTriangle(4, 5, 6);
-	Rectangle Rec(3, 5);
-	Square sq(4);
-	Circle cr(7.1);
-	Cube kocka(5);
-	cout << "First triangle perimeter: " << firstTriangle.calculatePerimeter() << endl;
-	cout << "Second triangle perimeter: " << secondTriangle.calculatePerimeter() << endl;
-	cout << "First triangle area: " << firstTriangle.calculateSurface() << endl;
-	cout << "Second triangle area: " << secondTriangle.calculateSurface() << endl;
-	cout << firstTriangle.typeOfTriangle() << endl;
-	cout << "Rectangle perimeter: " << Rec.calculatePerimeter() << endl;
-	cout << "Rectangle area: " << Rec.calculateSurface() << endl;
-	cout << "Square perimeter: " << sq.calculatePerimeter() << endl;
-	cout << "Square area: " << sq.calculateSurface() << endl;
-	cout << "Circle perimeter: " << cr.calculatePerimeter() << endl;
-	cout << "Circle area: " << cr.calculateSurface() << endl;
-	cout << "Cube perimeter: " << kocka.calculatePerimeter() << endl;
-	cout << "Cube area: " << kocka.calculateSurface() << endl;
-	cout << "Cube volume: " << kocka.calculateVolume() << endl;
+	printMenu();
+	int index = getUserChoice();
+	if (index < 1 || index > 5) { cout << "Index out of range"; return 0; }
+	switch (index) {
+	case 1:	printTriangleInfo(getTriangle()); break;
+	case 2: printRectangleInfo(getRectangle()); break;
+	case 3: printSquareInfo(getSquare()); break;
+	case 4: printCircleInfo(getCircle()); break;
+	case 5: printCubeInfo(getCube()); break;
+	}
 }
